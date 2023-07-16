22ce765ec571be84f31371c84d301ae7eb87e7d3
Matt Sheppard <msheppard@gmail.com>
2023-07-16 15:23:52 -0400

    loading random choices

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+import 'package:flutter/services.dart' show rootBundle;
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -30,16 +24,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -49,59 +34,101 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<String> filenames = [];
+  List<String> choices = [];
+
+  Future<List<String>> getAssetImagePaths() async {
+    List<String> imagePaths = [];
+
+    try {
+      final manifestContent = await rootBundle.loadString('AssetManifest.json');
+      final manifestMap = json.decode(manifestContent) as Map<String, dynamic>;
+
+      for (final path in manifestMap.keys) {
+        if (path.startsWith('images/') &&
+            (path.endsWith('.png') || path.endsWith('.JPG'))) {
+          imagePaths.add(path);
+          debugPrint(path);
+        }
+      }
+    } catch (e) {
+      print('Error loading asset manifest: $e');
+    }
+
+    return imagePaths;
+  }
+
+  void _setChoices() {
+    debugPrint('setting choices ${filenames.length}');
+
+    final random = Random();
+    choices = [];
+    while (choices.length < 4) {
+      final randomIndex = random.nextInt(filenames.length);
+      final randomString = filenames[randomIndex];
+
+      if (!choices.contains(randomString)) {
+        choices.add(randomString);
+        debugPrint(randomString);
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    debugPrint('in init function');
+    _initAsync();
+  }
+
+  Future<void> _initAsync() async {
+    await _loadAssetImagePaths();
+    _setChoices();
+    debugPrint('in init ${filenames.length}');
+  }
+
+  Future<void> _loadAssetImagePaths() async {
+    List<String> assetImagePaths = await getAssetImagePaths();
+    setState(() {
+      filenames = assetImagePaths;
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text('top'),
+            Image(image: AssetImage('images/Sheppard_Matt.JPG')),
+            Text('bottom'),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TextButton(
+                    onPressed: _incrementCounter,
+                    child: Text(filenames.isNotEmpty ? choices[0] : ''),
+                  ),
+                  TextButton(
+                    onPressed: _incrementCounter,
+                    child: Text(filenames.isNotEmpty ? choices[1] : ''),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Text('after row'),
           ],
         ),
       ),
@@ -109,7 +136,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
